9fb9b8a075ca40ca7a7fbc590d8b997587feec29
Jacob <jakeboone@aol.com>
2014-08-06 15:38:35 -0600

    appending countries in list form to page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var indexController = require('./controllers/index.js');
+var countries = require('./models/countries.json')
 
 var app = express();
 app.set('view engine', 'jade');
@@ -8,6 +9,12 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.get('/', indexController.index);
+
+app.get('/countries', function(req,res){
+	// res.send('C Test');
+	res.send(countries);
+});
+
 
 var server = app.listen(6577, function() {
 	console.log('Express server listening on port ' + server.address().port);
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,1 +1,34 @@
  console.log('TEST');
+
+
+var renderCountries = function(countries) {
+  // First, clear out the list in case it already has content
+  $('.load-countries').empty();
+
+  // Loop through all the countries and render a list item for them
+  for (var i = 0; i < countries.length; i++) {
+    $('.load-countries').append('<li class="countryList">' + 'Country:  ' + '<strong>'+ countries[i].name+'</strong>' + '  Region:  ' + '<strong>'+ countries[i].region +'</strong>'+'</li>');
+  }
+}
+
+$(function () {
+
+	 $('.load-countries').click(function(){
+	 console.log('Click Load Countries');
+	     // Make an ajax get request to pull down the list of countries
+	    $.get('/countries', function(countries) {
+	      // render the result
+	      renderCountries(countries);
+    });
+
+	 })
+
+
+
+
+
+
+
+
+
+ });
